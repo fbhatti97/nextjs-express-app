abae4f8885f869ecc1fe062cec5d89c92419b386
fbhatti97 <141708863+fbhatti97@users.noreply.github.com>
2024-07-17 12:38:25 +1000

    doink

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -3,9 +3,13 @@
 # Change ownership of the project directory to ec2-user
 sudo chown -R ec2-user:ec2-user /home/ec2-user/nextjs-express-app
 
+# Navigate to frontend and clean install
 cd /home/ec2-user/nextjs-express-app/frontend
+rm -rf node_modules package-lock.json
 npm install
 npm run build
 
+# Navigate to backend and clean install
 cd /home/ec2-user/nextjs-express-app/backend
+rm -rf node_modules package-lock.json
 npm install
--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -5,7 +5,7 @@ pkill node
 
 # Start the frontend
 cd /home/ec2-user/nextjs-express-app/frontend
-npm start &
+npm start -H 0.0.0.0 &
 
 # Start the backend
 cd /home/ec2-user/nextjs-express-app/backend
