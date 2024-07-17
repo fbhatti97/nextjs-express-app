d417d2b0d8f2c492a474183dea4b97c17bb9a0c9
fbhatti97 <141708863+fbhatti97@users.noreply.github.com>
2024-07-17 11:55:21 +1000

    blabla

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Change ownership of the project directory to ec2-user
+sudo chown -R ec2-user:ec2-user /home/ec2-user/nextjs-express-app
+
 cd /home/ec2-user/nextjs-express-app/frontend
 npm install
 npm run build
--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Stop any existing servers
 pkill node
 
